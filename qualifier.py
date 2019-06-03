7d162f65e37df885c13d9e7399dcfd0b3a34caf5
scOwez <owez@scalist.net>
2019-06-04 00:32:17 +0100

    Finished

--- qualifier.py
+++ qualifier.py
@@ -1,13 +1,43 @@
-
+import random
 
 def generate_password(
-    password_length: int = 8,
+    password_length: int = 1000000,
     has_symbols: bool = False,
-    has_uppercase: bool = False
+    has_uppercase: bool = False,
+    ignored_chars: list = [],
+    allowed_chars: list = []
 ) -> str:
     """Generates a random password.
-
     The password will be exactly `password_length` characters.
     If `has_symbols` is True, the password will contain at least one symbol, such as #, !, or @.
     If `has_uppercase` is True, the password will contain at least one upper case letter.
+    If `ignored_chars` is not None, characters on this list will be blacklisted
+    If `allowed_chars` is not None, only characters on this list will be used
+    If `ignored_chars` and `allowed_chars` are not None, `UserWarn()` will be raised
     """
+
+    if ignored_chars and allowed_chars:
+        raise UserWarning("Both ignored_chars & allowed_chars have been passed! Only 1 of these args are allowed.")
+
+    word_dict = "abcdefghijklmnopqrstuvwxyz"
+    
+    if has_uppercase:
+        word_dict += word_dict.upper()
+    elif has_symbols:
+        word_dict += "#!@"
+
+    output = ""
+
+    for i in range(password_length):
+        choice = random.choice(word_dict)
+
+        if allowed_chars:
+            while choice in ignored_chars or choice not in allowed_chars:
+                choice = random.choice(word_dict)
+        else:
+            while choice in ignored_chars:
+                choice = random.choice(word_dict)
+
+        output += choice
+
+    return output
